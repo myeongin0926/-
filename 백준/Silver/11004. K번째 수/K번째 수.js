42953dd2aa9e316658debb592ca3ce9304f1423b
유명인 <114664128+myeongin0926@users.noreply.github.com>
2023-07-13 10:56:55 +0900

    [Silver V] Title: K번째 수, Time: 4604 ms, Memory: 568564 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.js
+++ 백준/Silver/11004. K번째 수/K번째 수.js
@@ -5,13 +5,7 @@ const input = fs
   .trim()
   .split("\n");
 
-// const count = +input[0][2];
-// const arr = input[1].split(" ");
-
-// console.log(arr.sort((a, b) => a - b)[count - 1]);
-
-let [n, k] = input[0].split(" ").map(Number);
-let arr = input[1].split(" ").map(Number);
-
+const count = input[0].split(" ")[1];
+const arr = input[1].split(" ").map(Number);
 arr.sort((a, b) => a - b);
-console.log(arr[k - 1]);
+console.log(arr[count - 1]);
