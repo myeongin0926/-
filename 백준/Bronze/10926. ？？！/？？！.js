8c7018a9562d6c65b7ee99e338281475020bc56d
유명인 <114664128+myeongin0926@users.noreply.github.com>
2023-07-15 23:47:01 +0900

    [Bronze V] Title: ??!, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./test.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+console.log(input[0] + "??!");
