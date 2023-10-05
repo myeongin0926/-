01251667eaca6ecbec12b018a147332a38ce535c
유명인 <114664128+myeongin0926@users.noreply.github.com>
2023-10-05 17:39:22 +0900

    [unrated] Title: 과일 장수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,9 +1,7 @@
 function solution(k, m, score) {
     var answer = 0;
-    score.sort((a,b) => b>a ? 1 : -1)
-    for(let i = 0; i + m <= score.length; i += m) {
-        answer += Math.min(...score.slice(i, i+m)) * m
-    }
+    score.sort((a,b) => a < b ? 1 : -1)
+    for(let i = 0; i + m <= score.length; i += m) answer += score[i + m - 1] * m
     return answer
 }
 
