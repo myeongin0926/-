0f837402fd201a2ebb0c86f33135ef7b4bd66c25
유명인 <114664128+myeongin0926@users.noreply.github.com>
2023-10-06 22:21:00 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -0,0 +1,27 @@
+function bonus(num , type) {
+    return  Math.pow(num, type === 'S' ? 1 : type === 'D' ? 2 : 3);
+}
+
+function solution(dartResult) {
+    var answer = 0;
+    const arr = []
+    const numIdx = [0]
+    for(let i = 2; i < dartResult.length; i++) {
+        if(!isNaN(dartResult[i])){ 
+            arr.push(dartResult.slice(numIdx.at(-1), i))
+            numIdx.push(i)
+            i++
+        }
+    }
+    arr.push(dartResult.slice(numIdx.at(-1)))
+    const result = []
+    arr.forEach((el, idx) => {
+         const num = el.match(/\d+/g)[0]
+         const [type, option] = el.slice(num.length).split('')
+         const sum = bonus(num, type)
+         result.push(option === '*' ? sum * 2 : option === '#' ? -sum : sum)
+         if(option === '*') result[idx - 1] = result[idx-1] * 2
+    })
+    
+    return result.reduce((acc,cur) => acc += cur,0)
+}
