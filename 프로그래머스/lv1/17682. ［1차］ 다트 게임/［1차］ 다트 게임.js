976e7d7b5445440e14363dbd147688469b156bfd
유명인 <114664128+myeongin0926@users.noreply.github.com>
2023-10-06 22:21:46 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.34 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -1,27 +1,17 @@
-function bonus(num , type) {
-    return  Math.pow(num, type === 'S' ? 1 : type === 'D' ? 2 : 3);
-}
-
 function solution(dartResult) {
-    var answer = 0;
-    const arr = []
-    const numIdx = [0]
-    for(let i = 2; i < dartResult.length; i++) {
-        if(!isNaN(dartResult[i])){ 
-            arr.push(dartResult.slice(numIdx.at(-1), i))
-            numIdx.push(i)
-            i++
-        }
+    const bonus = { 'S': 1, 'D': 2, 'T': 3 },
+          options = { '*': 2, '#': -1, undefined: 1 };
+
+    let darts = dartResult.match(/\d.?\D/g);
+
+    for (let i = 0; i < darts.length; i++) {
+        let split = darts[i].match(/(^\d{1,})(S|D|T)(\*|#)?/),
+            score = Math.pow(split[1], bonus[split[2]]) * options[split[3]];
+
+        if (split[3] === '*' && darts[i - 1]) darts[i - 1] *= options['*'];
+
+        darts[i] = score;
     }
-    arr.push(dartResult.slice(numIdx.at(-1)))
-    const result = []
-    arr.forEach((el, idx) => {
-         const num = el.match(/\d+/g)[0]
-         const [type, option] = el.slice(num.length).split('')
-         const sum = bonus(num, type)
-         result.push(option === '*' ? sum * 2 : option === '#' ? -sum : sum)
-         if(option === '*') result[idx - 1] = result[idx-1] * 2
-    })
-    
-    return result.reduce((acc,cur) => acc += cur,0)
+
+    return darts.reduce((a, b) => a + b);
 }
