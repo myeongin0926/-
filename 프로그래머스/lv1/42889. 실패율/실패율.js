82c292a6909d195543cfc79230abafb5492ccca3
유명인 <114664128+myeongin0926@users.noreply.github.com>
2023-10-06 16:37:32 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,17 +1,9 @@
 function solution(N, stages) {
     var answer = [];
     for(let i = 1; i <= N; i++) {
-        let challengePeoples = 0
-        let failPeoples = 0
-        stages.forEach(el => {
-            if(el >= i) challengePeoples++
-            if(el === i) failPeoples++
-        })
-        answer[i] = [failPeoples / challengePeoples , i]
+        const challengePeoples = stages.filter(el => el >= i).length
+        let failPeoples = stages.filter(el => el === i).length
+        answer.push([failPeoples / challengePeoples , i])
     }
-    return answer.slice(1).sort((a,b) => {
-        if(a[0] > b[0]) return -1
-        else if(a[0] < b[0]) return 1
-        else if(a[0] === b[0]) return a[1] > b[1] ? 1 : -1
-    }).map(el => el[1])
+    return answer.sort((a,b) => b[0] - a[0]).map(el => el[1])
 }
