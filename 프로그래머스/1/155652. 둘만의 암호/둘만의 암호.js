f341592d62cdb4aea35c753828ad27c20f03041d
유명인 <114664128+myeongin0926@users.noreply.github.com>
2023-10-12 00:01:17 +0900

    [level 1] Title: 둘만의 암호, Time: 0.42 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,19 @@
+function solution(s, skip, index) {
+    const str = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+    var answer = '';
+    s = s.split('')
+    for(let i = 0; i < s.length; i++) {
+        const el = s[i]
+        let idx = str.indexOf(el) + 1
+        const arr = []
+        while(arr.length < index) {
+            if(!skip.includes(str[idx])) arr.push(str[idx])
+            idx++
+        }
+        if (arr.length) answer +=  arr.at(-1)
+    }
+
+    return answer;
+}
+
+
