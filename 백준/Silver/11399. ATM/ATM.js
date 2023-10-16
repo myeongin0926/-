73239998168c9af74918ff0af125f9f1eb65e93e
유명인 <114664128+myeongin0926@users.noreply.github.com>
2023-10-16 16:16:38 +0900

    [Silver IV] Title: ATM, Time: 128 ms, Memory: 9624 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.js
+++ 백준/Silver/11399. ATM/ATM.js
@@ -1,18 +1,18 @@
-const fs = require("fs");
-const input = fs
-  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./test.txt")
-  .toString()
-  .trim()
-  .split("\n");
 
-const data = input[1].split(" ").map(Number);
-data.sort((a, b) => a - b);
-let result = 0;
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-data.reduce((acc, cur) => {
-  acc += cur;
-  result += acc;
-  return acc;
-}, 0);
+let n = Number(input[0]);
+let arr = input[1].split(" ").map(Number);
 
-console.log(result);
+arr.sort((a, b) => a - b);
+
+let answer = 0;
+let summary = 0;
+
+for (let i = 0; i < n; i++) {
+  summary += arr[i];
+  answer += summary;
+}
+
+console.log(answer);
