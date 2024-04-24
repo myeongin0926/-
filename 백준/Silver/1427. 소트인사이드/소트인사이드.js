790e60f66401aa04ea143f894acb4eba301e6195
유명인 <114664128+myeongin0926@users.noreply.github.com>
2024-04-24 15:38:58 +0900

    [Silver V] Title: 소트인사이드, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.js
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.js
@@ -1,14 +1,18 @@
 const fs = require("fs");
-const input = fs
-  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./test.txt")
-  .toString()
-  .trim()
-  .split("\n");
+let input = fs.readFileSync("./dev/stdin").toString().split("\n");
 
-console.log(
-  input[0]
-    .split("")
-    .map(Number)
-    .sort((a, b) => b - a)
-    .join("")
-);
+let n = input[0];
+
+const cnt = new Array(10).fill(0);
+
+for (let x of n) {
+  cnt[Number(x)]++;
+}
+
+let answer = "";
+
+for (let i = 9; i >= 0; i--) {
+  if (cnt[i] !== 0) answer += String(i).repeat(cnt[i]);
+}
+
+console.log(answer);
