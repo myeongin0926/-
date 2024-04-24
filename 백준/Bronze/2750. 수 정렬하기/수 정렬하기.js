d562d509c829a9842a65162af021a18eeac24ee4
유명인 <114664128+myeongin0926@users.noreply.github.com>
2024-04-24 14:07:28 +0900

    [Bronze II] Title: 수 정렬하기, Time: 132 ms, Memory: 9756 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -1,12 +1,18 @@
 const fs = require("fs");
-const input = fs
-  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./test.txt")
-  .toString()
-  .trim()
-  .split("\n");
+let input = fs.readFileSync("./dev/stdin").toString().split("\n");
 
-input
-  .slice(1)
-  .map(Number)
-  .sort((a, b) => a - b)
-  .forEach((el) => console.log(el));
+const arr = [];
+const n = input[0];
+
+for (let i = 1; i <= n; i++) {
+  arr.push(+input[i]);
+}
+
+arr.sort((a, b) => a - b);
+let answer = "";
+
+for (let i = 0; i < arr.length; i++) {
+  answer += arr[i] + "\n";
+}
+
+console.log(answer);
