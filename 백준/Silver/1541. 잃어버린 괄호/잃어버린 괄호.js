1059bdaad574f2b902d74931afa7861927067e60
유명인 <114664128+myeongin0926@users.noreply.github.com>
2024-04-25 14:39:29 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 128 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
@@ -1,20 +1,16 @@
 const fs = require("fs");
-const input = fs
-  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./test.txt")
-  .toString()
-  .trim()
-  .split("\n");
+let input = fs.readFileSync("./dev/stdin").toString().split("\n");
 
+const string = input[0];
 
-const arr = input[0].split("-");
-let result = 0;
-for (let i = 0; i < arr.length; i++) {
-  let cur = arr[i]
-    .split("+")
-    .map(Number)
-    .reduce((acc, cur) => acc + cur);
-  if (i === 0) result += cur;
-  else result -= cur;
+const minusGroup = string.split("-");
+
+let answer = 0;
+
+for (let i = 0; i < minusGroup.length; i++) {
+  const sum = minusGroup[i].split("+").reduce((sum, num) => +sum + +num, 0);
+  if (i === 0) answer += sum;
+  else answer -= sum;
 }
 
-console.log(result);
+console.log(answer);
