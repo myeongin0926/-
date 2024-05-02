e5f40978bbde05ec6099951c0049a6516aad1d7b
유명인 <114664128+myeongin0926@users.noreply.github.com>
2024-05-02 15:55:46 +0900

    [Silver III] Title: 주유소, Time: 300 ms, Memory: 37472 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -8,19 +8,11 @@ const input = fs
 const street = input[1].split(" ").map(Number);
 const prices = input[2].split(" ").map(Number);
 
-let price = 0;
-let streetCur = 0;
-
-while (streetCur < street.length) {
-  if (prices[streetCur] < prices[streetCur + 1] && streetCur + 1 !== street.length) {
-    // 현재 주유소가 다음 주유소보다 가격이 싸다면
-    price += prices[streetCur] * (street[streetCur] + street[streetCur + 1]);
-    streetCur += 2;
-  } else {
-    price += prices[streetCur] * street[streetCur];
-    streetCur++;
-    // 현재 주유소가 다음 주유소보다 싸거나 같다면
-  }
+let result = BigInt(0);
+prices.forEach((el, i, arr) => {
+  if (arr[i] < arr[i + 1]) prices[i + 1] = prices[i];
+});
+for (let i = 0; i < prices.length - 1; i++) {
+  result += BigInt(prices[i]) * BigInt(street[i]);
 }
-
-console.log(price);
+console.log(String(result));
