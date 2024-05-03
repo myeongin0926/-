a9db677716541fd59806a677e800458d63bb00b2
유명인 <114664128+myeongin0926@users.noreply.github.com>
2024-05-03 15:31:03 +0900

    [Silver I] Title: 피보나치, Time: 276 ms, Memory: 11884 KB -BaekjoonHub

--- 백준/Silver/9009. 피보나치/피보나치.js
+++ 백준/Silver/9009. 피보나치/피보나치.js
@@ -1,23 +1,34 @@
-const fs = require("fs");
-const { EOL } = require("os");
-const input = fs
-  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./test.txt")
-  .toString()
-  .trim()
-  .split("\n");
+let fs = require("fs");
+let input = fs.readFileSync("./dev/stdin").toString().split("\n");
 
-const data = input.slice(1).map(Number);
-const fibo = data.map((el, i) => {
-  const fibo = [0, 1];
-  while (fibo.at(-1) + fibo.at(-2) <= data[i]) fibo.push(fibo.at(-1) + fibo.at(-2));
-  return fibo.sort((a, b) => b - a);
-});
-data.forEach((cur, i) => {
-  console.log(
-    fibo[i]
-      .filter((el) => (cur - el >= 0 ? ((cur -= el), true) : false))
-      .reverse()
-      .slice(1)
-      .join(" ")
-  );
-});
+const n = +input[0];
+const arr = input.slice(1).map(Number);
+
+// 피보 배열 설정
+const fibo = [0, 1];
+
+// 주어진 수 중에 가장 높은 수 저장 ( 피보 여러번 구하지 않기 위해 )
+const maxValue = Math.max(...arr);
+
+// 제일 높은 수까지 피보 배열 생성
+for (let i = 2; i < maxValue; i++) {
+  const sum = fibo[i - 1] + fibo[i - 2];
+  if (maxValue >= sum) {
+    fibo.push(sum);
+  } else break;
+}
+
+let result = [];
+
+for (let i = 0; i < n; i++) {
+  const queue = [];
+  for (let j = fibo.length; j > 0; j--) {
+    if (fibo[j] <= arr[i] && arr[i] > 0) {
+      queue.unshift(fibo[j]);
+      arr[i] = arr[i] - fibo[j];
+    }
+  }
+  result.push(queue);
+}
+
+result.forEach((el) => console.log(el.join(" ")));
