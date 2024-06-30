508812895e72fd05fdb2c6833d3c03cbf7d93a2e
유명인 <114664128+myeongin0926@users.noreply.github.com>
2024-06-30 17:56:02 +0900

    [Bronze II] Title: 문어 숫자, Time: 100 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/1864. 문어 숫자/문어 숫자.js
@@ -0,0 +1,32 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+input = input.map(str => str.trim()) 
+
+const oct = {
+    '-': 0,
+    '\\': 1,
+    '(': 2,
+    '@': 3,
+    '?': 4,
+    '>': 5,
+    '&': 6,
+    '%': 7,
+    '/': -1 
+}
+
+const arr = []
+for(let i = 0; i < input.length; i++) {
+    if(input[i] === '#') break
+    let cnt = 0
+    let total = 0
+    for(let j = input[i].length - 1; j >= 0; j--) {
+
+        total += oct[input[i][j]] * (8 ** cnt)
+        cnt++
+        // console.log(oct[input[i][j]])
+    }
+
+    arr.push(total)
+    // console.log(total)
+}
+
+console.log(arr.join('\n'))
